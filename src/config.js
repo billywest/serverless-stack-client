d9afffc08538dd6f5024d8474c4d380621531b77
Billy West <billywest.jm@gmail.com>
2020-09-23 01:18:03 -0400

    CRUD feature added

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: 'us-east-1',
     BUCKET: 'sirwest-notes-app-uploads',
@@ -11,6 +12,6 @@ export default {
     REGION: 'us-east-1',
     USER_POOL_ID: 'us-east-1_PscNpPQ64',
     APP_CLIENT_ID: '7fi755ahtq7ljk51dt3h9b0iic',
-    IDENTITY_POOL_ID: 'us-east-1:c8e4cec6-72e3-4f1d-96dd-ceb5fbae61ee',
+    IDENTITY_POOL_ID: 'us-east-1:52d00777-3b41-4499-9d33-7323959fe105',
   },
 };
---
+++ src/libs/errorLib.js
@@ -0,0 +1,11 @@
+export function onError(error) {
+  console.log(error);
+  let message = error.toString();
+
+  // Auth errors
+  if (!(error instanceof Error) && error.message) {
+    message = error.message;
+  }
+
+  alert(message);
+}
